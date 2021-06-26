7a1f9debab6bce546ba026a6a69a29cbb4e6b103
lackorol <57800924+lackorol@users.noreply.github.com>
2021-06-26 18:55:35 +0200

    Authentication - reactiveVariables

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReactiveVar } from "@apollo/client";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import { isLoggedInVar } from "./screens/apollo";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
 
 function App() {
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            {isLoggedIn ? <Home /> : <Login />}
+          </Route>
+          <Route path="/nomad-coders">
+            <h1>Potato</h1>
+          </Route>
+          <Route>
+            {/* <NotFound /> */}
+            <Redirect />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
